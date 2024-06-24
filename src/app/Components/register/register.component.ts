0c043d115ef9420107b667ceabf7483ff23c0cba
fatma <fatmamahrous1510@gmail.com>
2024-06-24 12:15:43 +0300

    register validation

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,3 +1,72 @@
+// import { Component, OnInit } from '@angular/core';
+// import {
+//   FormBuilder,
+//   FormControl,
+//   FormGroup,
+//   FormsModule,
+//   ReactiveFormsModule,
+//   Validators,
+// } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { CommonModule } from '@angular/common';
+// import { LoginComponent } from '../login/login.component';
+// import { Router, RouterModule } from '@angular/router';
+// import { HttpClientModule } from '@angular/common/http';
+// import { AccountService } from '../../Services/account.service';
+// import { data } from 'jquery';
+// import { RegisterUser } from '../../Models/RegisterUser';
+
+// @Component({
+//   selector: 'app-register',
+//   standalone: true,
+//   imports: [LoginComponent, RouterModule, HttpClientModule,FormsModule,CommonModule,ReactiveFormsModule],
+//   providers: [AccountService],
+//   templateUrl: './register.component.html',
+//   styleUrl: './register.component.css',
+// })
+// export class RegisterComponent {
+//   newUser: RegisterUser = {
+//     userName: "",
+//     email: "",
+//     password: "",
+//     confirmPassword: "",
+//     city: "",
+//     street: "",
+//     postalCode: "",
+//     phoneNumber: ""
+//   };
+//   res:any;
+//   constructor(public myService: AccountService, public router:Router) { }
+
+//  // Validation function for the username
+
+
+//   registerUser() {
+//     this.myService.Register(this.newUser).subscribe({
+//       next: (data) => {
+//       console.log('User registered successfully',data.body);
+//       this.res=data.body;
+//       this.Login();
+//       },
+//       error: (error) => {
+//       console.log("Error", error);
+//       }
+//     })
+//   }
+//   Login(){
+//     console.log("Navigate to login");
+    
+//     this.router.navigate(['/login']);
+//   }
+// }
+// /*response => {
+//       console.log('User registered successfully', response);
+//     }, error => {
+//       console.error('Error registering user', error);
+//     });*/
+
+
+
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -8,6 +77,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
 import { Router, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,7 +88,7 @@ import { RegisterUser } from '../../Models/RegisterUser';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [LoginComponent, RouterModule, HttpClientModule,FormsModule,ReactiveFormsModule],
+  imports: [LoginComponent, RouterModule, HttpClientModule,FormsModule,CommonModule,ReactiveFormsModule],
   providers: [AccountService],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
@@ -35,19 +105,48 @@ export class RegisterComponent {
     phoneNumber: ""
   };
   res:any;
+  errorMessage: string = '';
+
   constructor(public myService: AccountService, public router:Router) { }
+
+ // Validation function for the username
+
+
+  // registerUser() {
+  //   this.myService.Register(this.newUser).subscribe({
+  //     next: (data) => {
+  //     console.log('User registered successfully',data.body);
+  //     this.res=data.body;
+  //     this.Login();
+  //     },
+  //     error: (error) => {
+  //       // Handle error response
+  //       console.log("Error", error);
+        
+  //     }
+  //   })
+  // }
+
+
+
   registerUser() {
     this.myService.Register(this.newUser).subscribe({
       next: (data) => {
-      console.log('User registered successfully',data.body);
-      this.res=data.body;
-      this.Login();
+        console.log(data.body);
+        if (data.body === "Registration succeeded") {
+          this.Login();
+        } else {
+          this.errorMessage = data.body;
+        }
       },
       error: (error) => {
-      console.log("Error", error);
+        // Handle HTTP errors
+        console.error("Error", error);
+        this.errorMessage = 'Registration failed. Please try again.';
       }
-    })
+    });
   }
+
   Login(){
     console.log("Navigate to login");
     
