c2e11d322c0f23fe3b3669996672ab4e0a12c4a9
SonsonJimmy <Sonsongemy4@gmail.com>
2024-07-01 02:06:09 +0300

    updating profile service

--- src/app/Services/admin.service.ts
+++ src/app/Services/admin.service.ts
@@ -10,16 +10,15 @@ export class AdminService {
    private readonly DB_url = "https://localhost:7182/api/Admin";
 
    GetUnVerifiedOwners(){
-    return this.http.get(this.DB_url+"/GetUnVerifiedOwners",{observe:"response"}).pipe();
-    
+    return this.http.get(`${this.DB_url}/GetUnVerifiedOwners`,{observe:"response"}).pipe();
    }
 
-   ApproveOwner(){
-    return this.http.post(`${this.DB_url}+"/AppsroveOwner"}`,{observe:"response"}).pipe();
+   ApproveOwner(ownerId:number){
+    return this.http.post(`${this.DB_url}/ApproveOwner`,{observe:"response",params: { id: ownerId.toString() }}).pipe();
    }
 
-   DeclineOwner(){
-    return this.http.delete(this.DB_url+"/DeclineOwner",{observe:"response"}).pipe();
+   DeclineOwner(ownerId:number){
+    return this.http.delete(this.DB_url+"/DeclineOwner",{observe:"response",params: { id: ownerId.toString() },responseType:'text'});
    }
 
 }
